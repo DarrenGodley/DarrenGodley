2ec11bc38bf789e8a481ca4206f536b75496d671
t00187846 <darren.godley@students.ittralee.ie>
2017-10-03 15:36:18 +0100

    exercise1

--- problemSet1/src/Exercise1.java
+++ problemSet1/src/Exercise1.java
@@ -3,18 +3,17 @@ import javax.swing.*;
 public class Exercise1 {
     public static void main(String[] args) {
 
-        double exchangeRate, euro, pound;
-        int count=0;
+        double exchangeRate, euro, pound=1;
 
         exchangeRate = Double.parseDouble((JOptionPane.showInputDialog("Enter the exchange rate: ")));
 
-        while(count > 5){
+        while(pound != 0){
             pound = Integer.parseInt(JOptionPane.showInputDialog("enter amount in pounds"));
 
             euro = pound/exchangeRate;
 
-            JOptionPane.showMessageDialog(null, "Pound: " + pound + "exchangeRate" + exchangeRate +
-            "euro" + euro + String.format("%.2f"));
+            JOptionPane.showMessageDialog(null, String.format("Pounds: %1.2f\n Exchange Rate: %1.2f\n Euro: %1.2f", pound, exchangeRate, euro),
+                    "Money Converter", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 }
