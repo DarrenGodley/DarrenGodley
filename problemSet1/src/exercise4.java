df0604cf4c5a9192673c24252565ea2454f0d4f2
darrengodley <darren.godley@students.ittralee.ie>
2017-10-05 10:49:49 +0100

    exercise4 update

--- problemSet1/src/exercise4.java
+++ problemSet1/src/exercise4.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+
 public class exercise4 {
+    public static void main(String[] args) {
+
+        String fullName, capital;
+        int nameLength;
+        char first;
+
+
+        fullName = JOptionPane.showInputDialog("Please enter your full name");
+
+        nameLength = fullName.length();
+        first = fullName.charAt(0);
+        capital = fullName.toUpperCase();
+        
+
+    }
 }
